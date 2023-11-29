aeb1261daf6ccc010125ff43eefcfe3b0c81598b
the94zyskowski <maciej.zyskowski.professional@gmail.com>
2023-11-29 16:38:51 +0100

    Pole, field added. Preparation for making a gameboard/grid. One field can have terain type, optional item and optional multiple enemy of different types.

--- Bomba_Wpierdol_Ostateczny/Bomba_Wpierdol_Ostateczny.cpp
+++ Bomba_Wpierdol_Ostateczny/Bomba_Wpierdol_Ostateczny.cpp
@@ -3,14 +3,36 @@
 #include <string>
 #include <locale>
 #include <iostream>
+#include <vector>
+#include <optional>
+
+#include <memory> // Dla std::shared_ptr
+
+struct Pole {
+    enum class RodzajTerenu { Trawa, Piasek, Woda, Góry, Przepaść };
+    RodzajTerenu teren;
+    std::vector<std::shared_ptr<Postac>> przeciwnicy; // Lista przeciwników
+    std::optional<std::string> znalezionyPrzedmiot;
+
+    Pole(RodzajTerenu t, std::vector<std::shared_ptr<Postac>> p = {}, std::optional<std::string> z = {})
+        : teren(t), przeciwnicy(p), znalezionyPrzedmiot(z) {}
+};
+
+
+
 
 int main()
 {
     setlocale(LC_ALL, "Polish");
 
-    KapitanBomba tytus_bomba("Kapitan Tytus Bomba", 100, 9, 10, 7, 5, 5, 10);
+    // Tworzenie macierzy 10x10
+    std::vector<std::vector<Pole>> plansza(10, std::vector<Pole>(10, Pole(Pole::RodzajTerenu::Trawa)));
 
-    Kosmita kosmita_1("Michaś", 20, 3, 10, 7, 2, 3, 0);
+    // Ustawienie konkretnego pola z przeciwnikami
+    plansza[1][2] = Pole(Pole::RodzajTerenu::Piasek, {
+        std::make_shared<Kosmita>("Michaś", 20, 3, 7, 4, 3, 1, 0),
+        std::make_shared<Kosmita>("Zdzichu", 15, 5, 6, 3, 2, 2, 1)
+        });
 
 	return 0;
 }
